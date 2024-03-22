016410fc8ce8a780dd4ff762a9f4755fe09dfd10
Guilherme Santiago de Brito <guilhermesantiagodebritod.s@gmail.com>
2024-03-22 13:39:13 -0300

    finished

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [nome, setNome] = useState("");
+  // useEffect(() => {
+  //   alert("Bom dia!"), [nome];
+  // });
+  useEffect(() => {
+    if (nome.length === 30) {
+      alert("30 palavras");
+    }
+  });
+
+  Mudei = (nome) => {
+    setNome(`Bom dia ${nome} tudo beleza!`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Saudação: {nome}</Text>
+      <TextInput
+        placeholder="Digite seu nome:"
+        style={{ width: 300, height: 70 }}
+        onChangeText={Mudei}
+      />
     </View>
   );
 }
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
